dc95071565ca4efac660ff2b400e8aed7e5594f4
Roman Kuznetsov <Roma_kuznecov_2000@mail.ru>
2022-10-31 08:37:10 +0300

    graphQltest

--- server.js
+++ server.js
@@ -2,19 +2,38 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require('body-parser')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
 
-let port = 3000;
+const Event = require('./models/event')
+
+
 const app = express();
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events:[String!]!
+      events:[Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -23,16 +42,65 @@ app.use('/graphql', graphqlHTTP({
     }
   `),
   graphiql: true,
-  rootValue:{
-    events:(args) =>{
-      return ['cooking','Sails','Roman']
+  
+  rootValue: {
+
+
+    events: (args) => {
+      return Event.find().then(events => {
+        // console.log({...events})
+        // return events
+        return events.map(event => {
+          return { ...event._doc, _id: event.id }
+        })
+      }).catch(err => {
+        console.log(err)
+        throw err
+      })
     },
-    createEvent: (args) =>{
-      const eventName = args.name
-      return eventName
+
+    // return {...event._doc, _id:event.id}
+
+    createEvent: (args) => {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date)
+      })
+      return event
+        .save()
+        .then(result => {
+          console.log(result)
+          return { ...result._doc, _id: result._doc._id.toString() }
+        })
+        .catch(err => {
+          console.log(err)
+          throw err
+        })
     }
+
+
   }
 }));
 
-app.listen(port);
-console.log('GraphQL API server running at localhost:' + port);
+const DB_URL = `mongodb+srv://Romikr89:Romikrty2000@cluster0.bwkdp89.mongodb.net/?retryWrites=true&w=majority` //scum testNotConnect
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    app.listen(PORT)
+    console.log('GraphQL API server running at localhost:' + PORT)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
+
+
+
+
+
+
+
